94caf3af1629f791fbe01bf5f170f787ff855192
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-04-20 16:22:18 +0200

    responsive main completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,3 @@
-// SCOMMENTARE SE SI USA JQUERY
-// $(document).ready(function(){
-//
-// });
-
-// METODO JQUERY PER LA RICHIESTA DI API
-// $.ajax(
-// {
-// url: "https://flynn.boolean.careers/exercises/api/random/boolean",
-// method: "GET",
-// success: function (data, stato) {
-// $("#risultati").html(data);
-// },
-// error: function (richiesta, stato, errori) {
-// alert("E' avvenuto un errore. " + errore);
-// }
-// }
-// );
-
-// SCOMMENTARE SE SI USA VUE
 Vue.config.devtools = true;
 var app = new Vue({
   el: '#root',
